1583a25ea48e4ae9380021d3dcb10523d8cba020
Marc Reisner <reisner.marc@gmail.com>
2020-12-02 23:44:12 -0600

    Day 3 part 2

---
+++ day3_part2.py
@@ -0,0 +1,56 @@
+#!/usr/bin/env python
+
+from itertools import compress, cycle
+
+from aoc_input import read_input
+
+
+def main():
+    matrix = read_map_into_matrix()
+    answer = (
+        ski(matrix, 1, 1)
+        * ski(matrix, 3, 1)
+        * ski(matrix, 5, 1)
+        * ski(matrix, 7, 1)
+        * ski(matrix, 1, 2)
+    )
+    print(answer)
+
+
+def ski(matrix, right, down):
+    """
+    Ski down the slope in the matrix, using the provided slope.
+
+    Returns the number of trees hit.
+    """
+    matrix = [cycle(line) for line in matrix]
+    outer_selector = (i % down == 0 for i in range(len(matrix)))
+
+    column = 1
+    trees = 0
+
+    for row in compress(matrix, outer_selector):
+        inner_selector = (i == column - 1 for i in range(column))
+
+        col = next(compress(row, inner_selector))
+
+        if col == "#":
+            trees += 1
+
+        column += right
+
+    return trees
+
+
+def read_map_into_matrix():
+    """Read input into a usable map matrix."""
+    matrix = []
+
+    for line in read_input():
+        matrix.append(list(line))
+
+    return matrix
+
+
+if __name__ == "__main__":
+    main()
